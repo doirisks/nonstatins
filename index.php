0071e6f9d26376c4b35a98faabe3003f743ec83a
fyodr <tedmoriniv@gmail.com>
2016-08-01 10:05:36 -0400

    adjusted index.php to use the pure js interface in this branch

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html5>
 <html>
 <body>
-<?php 
-require("NNTcalc.php");
-?>
+<script src="NNTcalculator.js" type="text/javascript"></script>
+<div id="asdfasdf"></div>
+<script type="text/javascript">
+asdfasdf = new NNTcalculator("asdfasdf");
+</script>
 </body>
 
 </html>
